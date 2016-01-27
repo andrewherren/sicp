d899c98b79043bf8a74ee8e532f4a9818858b4e1
Drew Herren <andrewscottherren@gmail.com>
2016-01-27 00:10:29 -0500

    Updates to SICP Chapter 1

--- Chapter1.scm
+++ Chapter1.scm
@@ -352,13 +352,61 @@
 ;          integers in terms of adding, doubling, and halving and uses a
 ;          logarithmic number of steps.
 (define (fast-mult a b)
-  (fast-mult-iter 1 a b))
+  (fast-mult-iter 0 a b))
 (define (fast-mult-iter answer a b)
   (cond ((= b 0) answer)
-        ((even? b) (fast-mult-iter ; todo: figure out the first parameter in this iteration ;
-             a (/ b 2)))
-        (else (fast-mult-iter (+ answer a) a (- b 1)))))
+        ((even? b) (fast-mult-iter answer (double a) (/ b 2)))
+        (else (fast-mult-iter (+ a answer) a (- b 1)))))
 (define (even? n)
   (= (remainder n 2) 0))
 (define (double a)
   (* 2 a))
+
+; Exercise 1.19. There is a clever algorithm for computing the Fibonacci
+;          numbers in a logarithmic number of steps. Recall the transformation
+;          of the state variables a and b in the fib-iter process of section
+;          1.2.2: a a + b and b a. Call this transformation T, and observe
+;          that applying T over and over again n times, starting with 1 and 0,
+;          produces the pair Fib(n + 1) and Fib(n). In other words, the
+;          Fibonacci numbers are produced by applying Tn, the nth power of
+;          the transformation T, starting with the pair (1,0). Now consider T
+;          to be the special case of p = 0 and q = 1 in a family of
+;          transformations Tpq, where Tpq transforms the pair (a,b) according
+;          to a bq + aq + ap and b bp + aq. Show that if we apply such a
+;          transformation Tpq twice, the effect is the same as using a single
+;          transformation Tp'q' of the same form, and compute p' and q' in
+;          terms of p and q. This gives us an explicit way to square these
+;          transformations, and thus we can compute Tn using successive
+;          squaring, as in the fast-expt procedure. Put this all together
+;          to complete the following procedure, which runs in a logarithmic
+;          number of steps
+; (define (fib n)
+;   (fib-iter 1 0 0 1 n))
+; (define (fib-iter a b p q count)
+;   (cond ((= count 0) b)
+;         ((even? count)
+;          (fib-iter a
+;                    b
+;                    <??>      ; compute p'
+;                    <??>      ; compute q'
+;                    (/ count 2)))
+;         (else (fib-iter (+ (* b q) (* a q) (* a p))
+;                         (+ (* b p) (* a q))
+;                         p
+;                         q
+;                         (- count 1)))))
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   ; compute p'
+                   ; compute q'
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))

397bafa59d9a1d286ae8afd040c825d73bb3d3a9
Drew Herren <andrewscottherren@gmail.com>
2016-01-27 01:46:00 -0500

    Updates to SICP Chapter 1

--- Chapter1.scm
+++ Chapter1.scm
@@ -395,6 +395,19 @@
 ;                         p
 ;                         q
 ;                         (- count 1)))))
+
+; Tpq(a,b) => (bq + aq + ap, bp + aq)
+; Tpq(Tpq(a,b)) => Tpq(bq + aq + ap, bp + aq)
+;               => ((bp + aq)q + (bq + aq + ap)q + (bq + aq + ap)p,
+;                   (bp + aq)p + (bq + aq + ap)q)
+;               => (bpq + aq^2 + bq^2 + aq^2 + apq + bqp + aqp + ap^2,
+;                   bp^2 + aqp + bq^2 + aq^2 + apq)
+;               => (2aq^2 + bq^2 + 2apq + 2bqp + ap^2,
+;                   bp^2 + 2aqp + bq^2 + aq^2)
+;               => (b(q^2 + 2qp) + a(q^2 + 2qp) + a(q^2 + p^2),
+;                   b(p^2 + q^2) + a(q^2 + 2qp))
+; q <- q^2 + 2qp
+; p <- q^2 + p^2
 (define (fib n)
   (fib-iter 1 0 0 1 n))
 (define (fib-iter a b p q count)
@@ -402,8 +415,8 @@
         ((even? count)
          (fib-iter a
                    b
-                   ; compute p'
-                   ; compute q'
+                   (+ (square q) (square p))
+                   (+ (square q) (* 2 q p))
                    (/ count 2)))
         (else (fib-iter (+ (* b q) (* a q) (* a p))
                         (+ (* b p) (* a q))
